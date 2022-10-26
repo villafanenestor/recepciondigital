d0ffead39d2916bf249ea81354f38fcfb6902f0c
JuditasKid <samuellealrod@gmail.com>
2022-10-25 20:16:30 -0500

    Login page
    Pagina de logeo completa

--- frontend/src/componentes/Test.js
+++ frontend/src/componentes/Test.js
@@ -1,17 +1,31 @@
 import React from "react"
 import '../CSS/Test.css'
+import '../CSS/form.css'
 
  function Test() {
     return (
         <div className='contenedor-test'>
             <img className='imagen-test' src={require("../images/recep.jpeg")} alt='imagen test'/>
-            <div className='contenedor-texto-test'>
-                <p className="nombre-test">Sam el cachorro</p>
-                <p className="raza-test">Labrador</p>
-                <p className="texto-test">perrito melo callejero bien mamalon</p>
-            </div>
+             <form id="stripe-login"> 
+              <div className="">
+                <div className='field padding-bottom--24'>
+                    <label for="user">Usuario</label> <input type="user" name="usuario"></input><br></br>
+                        <div>
+                            <label for="password">Contraseña</label> <input type="password" name="Contraseña"></input>
+                        </div>
+                        <div class="reset-pass">
+                      <a href="#">Olvidaste tu contraseña?</a>
+                    </div>
+                        <input type="submit" name="submit" value="Continue"></input>
+                </div>
+              </div>
+             </form>
         </div>
+        
+        
     )
 }
+   
+
 
 export default Test;
