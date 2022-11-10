554f81b483d0d302ca28cbc8ba176d25a700b343
JuditasKid <samuellealrod@gmail.com>
2022-11-09 19:54:32 -0500

    ModuloAdmin
    Add admin module as css and js

--- frontend/src/componentes/admin.js
+++ frontend/src/componentes/admin.js
@@ -4,7 +4,18 @@ import "../CSS/Admin.css"
  function Admin() {
     return (
         <div className="contenedor-admin">
-            
+            <div className="propiedades">
+                <div class="btn-group">
+                    <button><a href="./admin.js">Propiedades</a></button>
+                    <button>Operador</button>
+                    <button>Registro Visitas</button>
+                </div>
+            </div>
+
+            <div className="logout">
+                <a href="#">Cerrar Sesion</a>
+
+            </div>
         </div>
     )
 }
