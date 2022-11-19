a996a0eedf4356a96a73613c22e2ddddc4205022
john lopez <johncharo@hotmail.com>
2022-11-18 21:49:17 -0500

    18/11/22
    no se hizo un culo.exe :(

--- frontend/src/componentes/Webs/admin.js
+++ frontend/src/componentes/Webs/admin.js
@@ -1,5 +1,6 @@
 import React from "react"
-import "../../CSS/Admin.css"
+import "../../CSS/Admin.css" 
+import {Container} from "react-bootstrap"
 
 
  function Admin() {
@@ -22,10 +23,25 @@ import "../../CSS/Admin.css"
                 <div className="Title">
                     <h1><strong>Registrar Propiedades</strong></h1> 
                 </div>
+                <div className="Container-Pro">
+                    <Container className="Data-1">
+                        Nombre <input type="text"  placeholder="Torre/Bloque"/>
+                    </Container>
+                    <Container className="Data-2">
+                        Cédula <input type="text"  placeholder="Torre/Bloque"/>
+                    </Container>
+                    <Container className="Data-3">
+                        Celular <input type="text"  placeholder="Torre/Bloque"/> 
+                    </Container>
+                    <Container className="Data-4">
+                        Torre/Bloque <input type="text"  placeholder="Torre/Bloque"/>
+                    </Container>
+                    <Container className="Data-">
+                        Apartamento/Casa <input type="text"  placeholder="Torre/Bloque"/>
+                    </Container>
+                 </div>
                 <div class="img">
-                    
                 </div>
-                
             </div>
         </div>   
     )
