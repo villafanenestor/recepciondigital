e54cf314fcd35e07548959b429844425429be250
JuditasKid <samuellealrod@gmail.com>
2022-11-22 20:46:08 -0500

    a

--- frontend/src/componentes/router/router.js
+++ frontend/src/componentes/router/router.js
@@ -18,6 +18,7 @@ export default function AppRouter() {
                 <PrivateRoute exact path={["/admins"]} component={AdminBuscar} />
                 
                 
+                
 
                 <Route path="/*" component={() => (
                     <h1>
